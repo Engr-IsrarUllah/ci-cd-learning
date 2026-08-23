9c3c9482f1d338d27e844bd9f142c05a637fd44d
Engr-IsrarUllah <engrisrar256@gmail.com>
2026-08-24 00:23:42 +0500

    Test failing CI

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,6 +2,6 @@ const app = require("../src/app");
 
 describe("Application", () => {
   test("application should exist", () => {
-    expect(app).toBeDefined();
+    expect(app).toBeUndefined();
   });
 });
